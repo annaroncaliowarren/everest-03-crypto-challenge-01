be94ba1be1399ef70f3d6fb3327af85e77a22293
Anna Roncalio <anna.roncalio@warren.com.br>
2022-10-06 10:51:48 -0300

    feat: add transaction model, provider and changed onPressed of button complete conversion in review screen

--- lib/screens/review/widgets/button_complete_conversion.dart
+++ lib/screens/review/widgets/button_complete_conversion.dart
@@ -1,17 +1,42 @@
+import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../confirmed_conversion/view/confirmed_conversion_screen.dart';
+import '../../conversion/provider/conversion_provider.dart';
+import '../../transactions/model/transaction_model.dart';
+import '../../transactions/provider/transactions_provider.dart';
 
 class ButtonCompleteConversion extends ConsumerWidget {
   const ButtonCompleteConversion({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final cryptoValueController =
+        ref.watch(textFieldCryptoControllerProvider.state);
+    final estimatedTotal = ref.watch(estimatedTotalProvider.state);
+    final conversionReal = ref.watch(conversionRealProvider.state);
+    final listTransactions = ref.watch(listTransactionsProvider.state);
+    final firstSelectedCrypto = ref.watch(firstSelectedCryptoProvider.state);
+    final secondSelectedCrypto = ref.watch(secondSelectedCryptoProvider.state);
+
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 24),
       child: MaterialButton(
         onPressed: () {
+          listTransactions.state.add(
+            TransactionModel(
+              fromCrypto: firstSelectedCrypto.state,
+              toCrypto: secondSelectedCrypto.state,
+              fromValueCrypto: Decimal.parse(
+                cryptoValueController.state.text.replaceAll(',', '.'),
+              ),
+              toValueCrypto: Decimal.parse(estimatedTotal.state.toString()),
+              dateConversion: DateTime.now(),
+              conversionReal: conversionReal.state,
+            ),
+          );
+
           Navigator.of(context).push(_createRouteAnimation());
         },
         height: 56,
